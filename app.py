7df3cf7ae7a5a0496809ba35d093402ff6aa80da
Zach Jacob <zachjacob96@gmail.com>
2026-07-21 15:42:57 -0400

    working streamlit app locally

---
+++ app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from datetime import date, timedelta
+from models import black_scholes
+
+def homepage():
+
+    # page text
+    st.title("Options Pricer")
+    st.write("Welcome to the homepage")
+
+    with st.form("options_form"):
+
+        # store user input variables
+        ticker = st.text_input("Underlying Ticker")
+
+        # enter strike price
+        k = st.text_input("Strike Price")
+        
+        # enter option type
+        cp = st.text_input("Call or Put?")
+
+        # get expiration date
+        expiry = st.text_input("Expiry", placeholder="mm/dd/yyyy")
+
+        submitted = st.form_submit_button("Calculate Options Price")
+
+    if submitted:
+
+        with st.spinner("Calculating..."):
+            ticker_init = yf.Ticker(ticker.upper()) # initialize ticker in yf
+
+            k = float(k) # convert string strike price to float
+
+            # get underlying stock price from yfinance api
+            s = ticker_init.history(period="1d")["Close"].iloc[-1]
+
+            cp = cp.lower() # standardize option type text format
+
+            # get annualized historical volatiliy
+            df = yf.download(ticker, period="1y") # get 1y price history
+            df["Log_Return"] = np.log(df["Close"] / df["Close"].shift(1)) # create new column documenting log returns
+            daily_vol = df["Log_Return"].std() # daily standard deviation
+            sigma = daily_vol * np.sqrt(252) # annualized volatility
+
+            # get dividend yield
+            q = ticker_init.info.get("dividendYield",0)
+            if q == None:
+                q = 0 # handling no dividend for calculation
+
+            # get time to expiry
+            today = date.today()
+            [month,day,year] = expiry.split("/")
+            t_days = (date(int(year),int(month),int(day)) - today) / timedelta(days=1)
+            t = t_days / 365
+
+            # get 3-year t-bond rate for risk-free rate
+            if t < 0.33:
+                treasury_ticker = "^IRX"   # 3-Month Treasury Bill
+            elif t < 5:
+                treasury_ticker = "^FVX"    # 5-Year Treasury Note
+            elif t < 10:
+                treasury_ticker = "^TNX"   # 10-Year Treasury Note
+            else:
+                treasury_ticker = "^TYX"    # 30-Year Treasury Bond
+            r = yf.Ticker(treasury_ticker).info.get("previousClose") / 100
+
+            # run black-scholes with inputted variables
+            options_price = black_scholes(k,s,r,sigma,t,q,cp)
+        st.success(options_price)
+
+if __name__ == "__main__":
+    homepage()
